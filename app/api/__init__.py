57d4f64c9504570e3247673565908462f85d6e81
Himeno-Sena258 <himenosena258@outlook.com>
2025-10-20 16:55:27 +0800

    feat: add CRUD to express module

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -3,6 +3,7 @@ from app.api.routes import app as user_router
 from app.api.session_routes import router as session_router
 from app.api.appointment_routes import router as appointment_router
 from app.api.task_routes import router as task_router
+from app.api.express_routes import router as express_router
 
 router = APIRouter()
 
@@ -17,3 +18,6 @@ router.include_router(appointment_router)
 
 # 注册任务相关路由
 router.include_router(task_router)
+
+# 注册快递相关路由
+router.include_router(express_router)
