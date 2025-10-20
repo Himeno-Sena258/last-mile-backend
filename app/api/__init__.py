45f312b6d9c4bd574fe413f0d0b3ed4e3e3e527e
Himeno-Sena258 <himenosena258@outlook.com>
2025-10-20 16:31:31 +0800

    feat: add CRUD to appointment module

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 from app.api.routes import app as user_router
 from app.api.session_routes import router as session_router
+from app.api.appointment_routes import router as appointment_router
 
 router = APIRouter()
 
@@ -9,3 +10,6 @@ router.include_router(user_router)
 
 # 注册会话相关路由
 router.include_router(session_router)
+
+# 注册预约相关路由
+router.include_router(appointment_router)
