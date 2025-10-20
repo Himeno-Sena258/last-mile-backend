50dc1f5de30223a3e3668500a15d7643bbed49e1
Himeno-Sena258 <himenosena258@outlook.com>
2025-10-20 16:49:56 +0800

    feat: add CRUD to task module

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 from app.api.routes import app as user_router
 from app.api.session_routes import router as session_router
 from app.api.appointment_routes import router as appointment_router
+from app.api.task_routes import router as task_router
 
 router = APIRouter()
 
@@ -13,3 +14,6 @@ router.include_router(session_router)
 
 # 注册预约相关路由
 router.include_router(appointment_router)
+
+# 注册任务相关路由
+router.include_router(task_router)
